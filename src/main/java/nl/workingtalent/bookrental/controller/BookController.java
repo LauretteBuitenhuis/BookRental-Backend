c2c76e6c0659e548988ff7c5ea2de2f578d8f5de
Laurette Buitenhuis <laurettebuitenhuis@hotmail.com>
2022-11-30 14:14:36 +0100

    Book delete and edit added

--- src/main/java/nl/workingtalent/bookrental/controller/BookController.java
+++ src/main/java/nl/workingtalent/bookrental/controller/BookController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,9 +32,27 @@ public class BookController {
 		return newBook;
 	}
 	
+	@DeleteMapping("book/{id}/delete")
+	public void delete(@PathVariable long id) {
+		repo.deleteById(id);
+	}
+	
+	@PostMapping("book/{id}/edit")
+	public void editBook(@RequestBody Book book, @PathVariable long id) {
+		Book prevBook = repo.findById(id).get();
+		
+		prevBook.setAuthor(book.getAuthor());
+		prevBook.setIsbn(book.getIsbn());
+		prevBook.setTitle(book.getTitle());
+		
+		repo.save(prevBook);
+		
+	}
+	
 	@GetMapping("book/all")
 	public List<Book> findAllBooks(){
 		return repo.findAll();
 	}
-
+	
+	
 }
