72c841c6ce8899c5d98db741bd0264b2055cc976
Ravi Mohanlal <ravimohanlal@hotmail.com>
2022-11-30 13:33:41 +0100

    Tested book

--- src/main/java/nl/workingtalent/bookrental/model/Book.java
+++ src/main/java/nl/workingtalent/bookrental/model/Book.java
@@ -21,5 +21,37 @@ public class Book {
 
 	@Column(nullable = false, length = 100)
 	private String author;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 	
 }
