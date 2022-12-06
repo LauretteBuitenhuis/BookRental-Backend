c098c3dd40cae959c32c20e743a282c025b343df
Laurette Buitenhuis <laurettebuitenhuis@hotmail.com>
2022-12-06 14:31:02 +0100

    Compare email & password on same id

--- src/main/java/nl/workingtalent/bookrental/controller/UserController.java
+++ src/main/java/nl/workingtalent/bookrental/controller/UserController.java
@@ -2,6 +2,7 @@ package nl.workingtalent.bookrental.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,4 +21,16 @@ public class UserController {
 	public void createUser(@RequestBody User user) {
 		repo.save(user);
 	}
+	
+	@GetMapping("user/login")
+	public boolean findUserByEmail() {
+		System.out.println("Werkt!");
+		User userInlogMail=repo.findByEmail("12345@gmail.com");
+		System.out.println("Id belonging to email is: " + userInlogMail.getId());
+		User userInlogPassword=repo.findByPassword("welkom12345");
+		System.out.println("Id belonging to password is: "+ userInlogPassword.getId());
+		return userInlogPassword.getId() == userInlogMail.getId();
+	
+	}
+	
 }
--- src/main/java/nl/workingtalent/bookrental/repository/IUserRepository.java
+++ src/main/java/nl/workingtalent/bookrental/repository/IUserRepository.java
@@ -1,8 +1,10 @@
 package nl.workingtalent.bookrental.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import nl.workingtalent.bookrental.model.User;
 
 public interface IUserRepository extends JpaRepository<User, Long>{
-
+	User findByEmail(String email);
+	User findByPassword(String password);
 }
