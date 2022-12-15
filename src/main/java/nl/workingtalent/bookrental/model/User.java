3a5f42fc7f1ae73bc1f3daf63d8ad72112e7c50e
Laurette Buitenhuis <laurettebuitenhuis@hotmail.com>
2022-12-15 10:29:57 +0100

    Try pushing

--- src/main/java/nl/workingtalent/bookrental/model/User.java
+++ src/main/java/nl/workingtalent/bookrental/model/User.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class User {
 
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -32,7 +33,7 @@ public class User {
 	
 	@Column(nullable = false)
 	private boolean admin;
-	
+	//hoi
 	@Column(nullable = false)
 	private boolean enabled;
 	
