9e7d04e15405598b23ba7f597364b26f8cc99d3d
Laurette Buitenhuis <laurettebuitenhuis@hotmail.com>
2023-01-05 13:14:25 +0100

    added deleteUser function

--- src/main/java/nl/workingtalent/bookrental/controller/UserController.java
+++ src/main/java/nl/workingtalent/bookrental/controller/UserController.java
@@ -12,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -188,5 +190,15 @@ public class UserController {
 	@GetMapping("user/all")
 	public List<User> findAllUsers(){
 		return userRepo.findAll();
+	}
+	
+	@DeleteMapping("user/{id}/delete")
+	public void delete(@RequestHeader(name = "Authorization") String token, @PathVariable long id) {
+
+		if (!userIsAdmin(token)) {
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid permissions for deleting book");
+		}
+
+		userRepo.deleteById(id);
 	}
 }
