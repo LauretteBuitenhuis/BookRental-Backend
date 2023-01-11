21aae9c18f62e82eac40bb3718b764c796c496b3
lisalootens <lisa_lootens@live.nl>
2023-01-11 15:24:56 +0100

    Check if password meets the requirements with regex

--- src/main/java/nl/workingtalent/bookrental/services/ValidatePassword.java
+++ src/main/java/nl/workingtalent/bookrental/services/ValidatePassword.java
@@ -1,16 +1,27 @@
 package nl.workingtalent.bookrental.services;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Service;
+
 import nl.workingtalent.bookrental.dto.NewUserDto;
 
+@Service
 public class ValidatePassword {
 	
 	private String ENTERED_PASSWORD;
-	private String ALLOWED_CHARS = "1234567890";
+	// TODO - add special character
+	// TODO - add minimum length
+	private String REGEX = "^(?=.*\\p{Upper})(?=.*[a-zA-Z])(?=.*[0-9]).*[a-zA-Z0-9]*$";
 	
 	public boolean passwordIsValid(NewUserDto newUserDto) {
 		ENTERED_PASSWORD = newUserDto.getPassword();
 		
-		return true;
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(ENTERED_PASSWORD);
+        
+        return matcher.matches();
 	}
-
+	
 }
