a6b3d8871fce4e737989114203fc8f8881580e51
lisalootens <lisa_lootens@live.nl>
2023-01-11 15:35:19 +0100

    Refactor code: remove globals

--- src/main/java/nl/workingtalent/bookrental/services/ValidatePassword.java
+++ src/main/java/nl/workingtalent/bookrental/services/ValidatePassword.java
@@ -2,23 +2,18 @@ package nl.workingtalent.bookrental.services;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.springframework.stereotype.Service;
-
 import nl.workingtalent.bookrental.dto.NewUserDto;
 
 @Service
 public class ValidatePassword {
-	
-	private String ENTERED_PASSWORD;
-	private String REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
 
-	
 	public boolean passwordIsValid(NewUserDto newUserDto) {
-		ENTERED_PASSWORD = newUserDto.getPassword();
+		String enteredPassword = newUserDto.getPassword();
+		String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
 		
-        Pattern pattern = Pattern.compile(REGEX);
-        Matcher matcher = pattern.matcher(ENTERED_PASSWORD);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(enteredPassword);
         
         return matcher.matches();
 	}
